bbda4a7a134d922ef87cf8d3b50d665c48e8f096
Dmitry Kostrikov <DaimonUA@gmail.com>
2019-03-20 23:45:04 +0200

    +HomeWork.Delegate.HunterAndHare

--- ConsoleApp2/Hunter.cs
+++ ConsoleApp2/Hunter.cs
@@ -6,16 +6,25 @@ namespace HomeWork.Delegate.HunterAndHare
 {
     class Hunter
     {
+        public string name;
         public delegate void Moved(Hare ha);
-        public string Name { get; } 
+        public string Name { get; }
         public Hunter(string name)
 
         {
             Name = name;
+            this.name = name;
         }
+        //реализация анономного метода
+        public Moved dlampda = (Hare m)=> Console.WriteLine("--лямпда выражение--" + m.Name + ": " + m.X.ToString() + "/" + m.Y.ToString());
+        public Moved d = delegate (Hare m)
+        {
+            //Console.WriteLine(name+ "---" + m.Name + ": " + m.X.ToString() + "/" + m.Y.ToString());
+            Console.WriteLine("--анонимный метод--" + m.Name + ": " + m.X.ToString() + "/" + m.Y.ToString());
+        };
         public void SendMessage(Hare hare)
         {
-            Console.WriteLine(Name+"---"+hare.Name + ": " + hare.X.ToString() + "/" + hare.Y.ToString());
+            Console.WriteLine(Name+"--делегат через событие-"+hare.Name + ": " + hare.X.ToString() + "/" + hare.Y.ToString());
         }
     }
 
