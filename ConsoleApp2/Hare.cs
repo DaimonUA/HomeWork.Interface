34a0d870cff8473e0c79f52091d39de818926677
Dmitry Kostrikov <DaimonUA@gmail.com>
2019-03-20 22:45:17 +0200

    +HomeWork.Delegate.HunterAndHare

---
+++ ConsoleApp2/Hare.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork.Delegate.HunterAndHare
+{
+    class Hare
+    {
+        private int x;
+        private int y;
+        public event Hunter.Moved Moveding;
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public string Name { get; private set; }
+        public Hare(string name)
+        {
+            Name = name;
+
+            Move();
+        }
+
+        public void Move()
+        {
+            Random rnd = new Random();
+            x = rnd.Next(100);
+            y = rnd.Next(100);
+            X = x;
+            Y = y;
+            if (Moveding != null)
+            {
+                //Console.WriteLine("Координаты зайца: %1 %2",x,y);
+                Moveding(this);
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Hunter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork.Delegate.HunterAndHare
+{
+    class Hunter
+    {
+        public delegate void Moved(Hare ha);
+        public string Name { get; } 
+        public Hunter(string name)
+
+        {
+            Name = name;
+        }
+        public void SendMessage(Hare hare)
+        {
+            Console.WriteLine(Name+"---"+hare.Name + ": " + hare.X.ToString() + "/" + hare.Y.ToString());
+        }
+    }
+
+}
